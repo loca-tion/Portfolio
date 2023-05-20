1681d7039e47c625582722b6b80613ddc6c739b4
Sujal Sharma <95977443+loca-tion@users.noreply.github.com>
2023-05-20 14:41:15 +0530

    Add files via upload

---
+++ src/routes/ProjectsPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "../Components/Navbar";
+import Projects from '../Components/Projects';
+
+
+function ProjectsPage() {
+  return (
+    <>
+    <Navbar />
+    <Projects />
+    </>
+  )
+}
+
+export default ProjectsPage
