1e558d62db2be0d04cd3b8c4384c063b72c7f731
Sujal Sharma <95977443+loca-tion@users.noreply.github.com>
2023-09-20 01:30:25 +0530

    Add files via upload

---
+++ src/routes/Resume.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "../Components/Navbar";
+import SkillSection from '../Components/SkillSection';
+import ResumeSection from '../Components/ResumeSection';
+
+
+function Resume() {
+  return (
+    <>
+    <Navbar />
+    <SkillSection />
+    <ResumeSection />
+    </>
+  )
+}
+
+export default Resume
